d7ade4846ae12827970b3ee17ca56d510bf472a8
Mads Ulrik Svendsen <mail@madsulrik.com>
2015-10-20 01:56:46 +0200

    Finally got my org-journal stuff like i would like it to.
    Now the only thing left is to break the org stuff out into a separat file and also fix a bug when you open the journal org file i narrow to the settings instead of the correct date section.

--- init.el
+++ init.el
@@ -27,6 +27,7 @@
                         gist
                         graphviz-dot-mode
                         magit
+			org-journal
                         marmalade
 			python-mode
                         solarized-theme
@@ -155,6 +156,7 @@
 ;; 
 ;; org mode setup
 ;;
+
 (require 'ox-latex)
 (add-to-list 'org-latex-classes
           '("org-noter"
@@ -187,6 +189,58 @@
 (setq org-confirm-babel-evaluate nil)
 
 (setq org-export-with-section-numbers nil)
+
+;; youtube tricks for org-mode
+(defvar yt-iframe-format
+  ;; You may want to change your width and height.
+  (concat "<iframe width=\"720\""
+          " height=\"480\""
+          " src=\"https://www.youtube.com/embed/%s\""
+          " frameborder=\"0\""
+          " allowfullscreen>%s</iframe>"))
+
+(org-add-link-type
+ "yt"
+ (lambda (handle)
+   (browse-url
+    (concat "https://www.youtube.com/embed/"
+            handle)))
+ (lambda (path desc backend)
+   (cl-case backend
+     (html (format yt-iframe-format
+                   path (or desc "")))
+     (latex (format "\href{%s}{%s}"
+                    path (or desc "video"))))))
+
+;; org journal stuff
+(defvar org-journal-file "~/Documents/.journal/journal.org"
+  "Path to OrgMode journal file.")
+(defvar org-journal-date-format "%A, %d-%m-%Y"
+  "Date format string for journal headings.")
+
+(defun org-journal-entry ()
+  "Create a new diary entry for today or append to an existing one."
+  (interactive)
+  (switch-to-buffer (find-file org-journal-file))
+  (widen)
+  (setq isearch-forward t)
+  (let ((today (format-time-string org-journal-date-format)))
+    (beginning-of-buffer)
+    (unless (org-goto-local-search-headings today nil t)
+      ((lambda ()
+         (org-insert-heading)
+         (insert today)
+         (insert "\n"))))
+    (beginning-of-buffer)
+    (org-show-entry)
+    (org-narrow-to-subtree)
+    (end-of-buffer)
+    (backward-char 1)
+    (unless (= (current-column) 2)
+      (insert "\n\n "))))
+
+(global-set-key (kbd "C-c j") 'org-journal-entry)
+
 
 ;; less Mode
 (require 'less-css-mode)
