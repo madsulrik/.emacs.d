83be57836a0d2c6a06313366e8ffd2eb704dd5d3
Mads Ulrik Svendsen <mail@madsulrik.com>
2016-02-27 17:06:51 +0100

    Line number font scaling issue work around

--- config/general-config.el
+++ config/general-config.el
@@ -66,6 +66,17 @@
 
 ;; line numbers and column numbers
 (require 'linum)
+(defun linum-update-window-scale-fix (win)
+  "fix linum for scaled text"
+  (set-window-margins win
+          (ceiling (* (if (boundp 'text-scale-mode-step)
+                  (expt text-scale-mode-step
+                    text-scale-mode-amount) 1)
+              (if (car (window-margins))
+                  (car (window-margins)) 1)
+              ))))
+(advice-add #'linum-update-window :after #'linum-update-window-scale-fix)
+
 (global-linum-mode t)
 (setq column-number-mode t)
 
